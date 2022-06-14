5d997cdd1f77651fa9151f4af98efe66af7d24e1
Songki Choi <songki.choi@intel.com>
2022-06-14 13:39:25 +0900

    Enable MPA on SC (#17)
    * Update CHANGELOG.md (from release branch)
    * Separate mmcls/det/seg dependencies & module register
    * Remove standalone venv

--- mpa/modules/datasets/__init__.py
+++ mpa/modules/datasets/__init__.py
@@ -3,17 +3,3 @@
 #
 
 # flake8: noqa
-from . import cls_csv_dataset
-from . import cls_csv_incr_dataset
-from . import cls_dir_dataset
-from . import det_csv_dataset
-from . import det_incr_dataset
-from . import multi_cls_dataset
-from . import task_adapt_dataset
-from . import seg_incr_cityscapes_dataset
-from . import seg_incr_voc_dataset
-from . import seg_task_adapt_dataset
-from . import pseudo_balanced_dataset
-
-from . import pipelines
-from . import samplers
--- mpa/modules/optimizer/__init__.py
+++ mpa/modules/optimizer/__init__.py
@@ -3,4 +3,3 @@
 #
 
 # flake8: noqa
-from . import lars
