bd8ab89a0d714ad7791ec5c172d4d63b3d45684d
Eugene Liu <eugene.liu@intel.com>
2022-07-19 10:18:43 +0100

    add EigenCAM implementation

--- mpa/modules/hooks/auxiliary_hooks.py
+++ mpa/modules/hooks/auxiliary_hooks.py
@@ -16,6 +16,54 @@ from __future__ import annotations
 import torch
 
 
+class EigenCamHook:
+    def __init__(self, module: torch.nn.Module) -> None:
+        self._module = module
+        self._handle = None
+        self._records = []
+
+    @property
+    def records(self):
+        return self._records
+
+    @staticmethod
+    def func(x: torch.Tensor) -> torch.Tensor:
+        bs, c, h, w = x.size()
+        reshaped_fmap = x.reshape((bs, c, h * w)).transpose(1, 2)
+        reshaped_fmap = reshaped_fmap - reshaped_fmap.mean(1)[:, None, :]
+        U, S, V = torch.linalg.svd(reshaped_fmap, full_matrices=True)
+        saliency_map = (reshaped_fmap @ V[:, 0][:, :, None]).squeeze(-1)
+        max_values, _ = torch.max(saliency_map, -1)
+        min_values, _ = torch.min(saliency_map, -1)
+        saliency_map = (
+            255
+            * (saliency_map - min_values[:, None])
+            / ((max_values - min_values + 1e-12)[:, None])
+        )
+        saliency_map = saliency_map.reshape((bs, h, w))
+        saliency_map = saliency_map.to(torch.uint8)
+        return saliency_map
+
+    def _recording_forward(
+        self, _: torch.nn.Module, input: torch.Tensor, output: torch.Tensor
+    ) -> torch.Tensor:
+
+        saliency_map = self.func(output)
+        saliency_map = saliency_map.detach().cpu().numpy()
+        if len(saliency_map) > 1:
+            for tensor in saliency_map:
+                self._records.append(tensor)
+        else:
+            self._records.append(saliency_map)
+
+    def __enter__(self) -> SaliencyMapHook:
+        self._handle = self._module.register_forward_hook(self._recording_forward)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._handle.remove()
+
+
 class SaliencyMapHook:
     def __init__(self, module: torch.nn.Module) -> None:
         self._module = module
