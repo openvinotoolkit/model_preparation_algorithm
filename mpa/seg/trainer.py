8efcfa1fdb2febb8809aea11409e1ed66942f1e9
Jihwan Eom <jihwan.eom@intel.com>
2022-08-31 14:50:23 +0900

    Resolve segmentation cpu training issue

--- mpa/seg/trainer.py
+++ mpa/seg/trainer.py
@@ -2,163 +2,209 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import os
-import time
-import numbers
-import glob
+# Copyright (c) OpenMMLab. All rights reserved.
+import random
+import warnings
+
+import numpy as np
 import torch
-import torch.multiprocessing as mp
-import torch.distributed as dist
-
-import mmcv
-from mmcv import get_git_hash
-
-from mmseg import __version__
-# from mmseg.apis import train_segmentor
-from .train import train_segmentor
-# from mmseg.datasets import build_dataset
+from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
+from mmcv.parallel.data_container import DataContainer
+from mmcv.runner import Fp16OptimizerHook, build_optimizer, build_runner
+from mmseg.core import DistEvalHook, EvalHook
+# from mmdet.core import DistEvalHook, EvalHook
+# from mmseg.datasets import build_dataloader, build_dataset
+from mmseg.models import build_params_manager
+from mmseg.datasets import build_dataloader
 from .builder import build_dataset
-from mmseg.models import build_segmentor
-from mmseg.utils import collect_env
-
-from mpa.registry import STAGES
-from mpa.seg.stage import SegStage
-from mpa.utils.logger import get_logger
-
-logger = get_logger()
+from mmseg.utils import get_root_logger
 
 
-@STAGES.register_module()
-class SegTrainer(SegStage):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+def set_random_seed(seed, deterministic=False):
+    """Set random seed.
+    Args:
+        seed (int): Seed to be used.
+        deterministic (bool): Whether to set the deterministic option for
+            CUDNN backend, i.e., set `torch.backends.cudnn.deterministic`
+            to True and `torch.backends.cudnn.benchmark` to False.
+            Default: False.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
-    def run(self, model_cfg, model_ckpt, data_cfg, **kwargs):
-        """Run training stage for segmentation
 
-        - Configuration
-        - Environment setup
-        - Run training via MMSegmentation -> MMCV
-        """
-        self._init_logger()
-        mode = kwargs.get('mode', 'train')
-        if mode not in self.mode:
-            return {}
+def train_segmentor(model,
+                    dataset,
+                    cfg,
+                    distributed=False,
+                    validate=False,
+                    timestamp=None,
+                    meta=None):
+    """Launch segmentor training."""
+    logger = get_root_logger(cfg.log_level)
 
-        cfg = self.configure(model_cfg, model_ckpt, data_cfg, **kwargs)
+    # prepare data loaders
+    dataset = dataset if isinstance(dataset, (list, tuple)) else [dataset]
+    data_loaders = [
+        build_dataloader(
+            ds,
+            cfg.data.samples_per_gpu,
+            cfg.data.workers_per_gpu,
+            # cfg.gpus will be ignored if distributed
+            len(cfg.gpu_ids),
+            dist=distributed,
+            seed=cfg.seed,
+            drop_last=False,
+            ) for ds in dataset
+    ]
 
-        if cfg.runner.type == 'IterBasedRunner':
-            cfg.runner = dict(type=cfg.runner.type, max_iters=cfg.runner.max_iters)
-
-        logger.info('train!')
-
-        # Work directory
-        mmcv.mkdir_or_exist(os.path.abspath(cfg.work_dir))
-
-        timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
-
-        # Environment
-        distributed = False
-        if cfg.gpu_ids is not None:
-            if isinstance(cfg.get('gpu_ids'), numbers.Number):
-                cfg.gpu_ids = [cfg.get('gpu_ids')]
-            if len(cfg.gpu_ids) > 1:
-                distributed = True
-        logger.info(f'cfg.gpu_ids = {cfg.gpu_ids}, distributed = {distributed}')
-        env_info_dict = collect_env()
-        env_info = '\n'.join([(f'{k}: {v}') for k, v in env_info_dict.items()])
-        dash_line = '-' * 60 + '\n'
-        logger.info('Environment info:\n' + dash_line + env_info + '\n' + dash_line)
-
-        # Data
-        datasets = [build_dataset(cfg.data.train)]
-
-        # Dataset for HPO
-        hp_config = kwargs.get('hp_config', None)
-        if hp_config is not None:
-            import hpopt
-
-            if isinstance(datasets[0], list):
-                for idx, ds in enumerate(datasets[0]):
-                    datasets[0][idx] = hpopt.createHpoDataset(ds, hp_config)
-            else:
-                datasets[0] = hpopt.createHpoDataset(datasets[0], hp_config)
-
-        # Target classes
-        if 'task_adapt' in cfg:
-            target_classes = cfg.task_adapt.final
+    # put model on gpus
+    if torch.cuda.is_available():
+        if distributed:
+            find_unused_parameters = cfg.get('find_unused_parameters', False)
+            # Sets the `find_unused_parameters` parameter in
+            # torch.nn.parallel.DistributedDataParallel
+            model = MMDistributedDataParallel(
+                model.cuda(),
+                device_ids=[torch.cuda.current_device()],
+                broadcast_buffers=False,
+                find_unused_parameters=find_unused_parameters)
         else:
-            target_classes = datasets[0].CLASSES
+            model = MMDataParallel(
+                model.cuda(cfg.gpu_ids[0]), device_ids=cfg.gpu_ids)
+    else:
+        model = MMDataCPU(model)
 
-        # Metadata
-        meta = dict()
-        meta['env_info'] = env_info
-        # meta['config'] = cfg.pretty_text
-        meta['seed'] = cfg.seed
-        meta['exp_name'] = cfg.work_dir
-        if cfg.checkpoint_config is not None:
-            cfg.checkpoint_config.meta = dict(
-                mmseg_version=__version__ + get_git_hash()[:7],
-                CLASSES=target_classes)
+    # build runner
+    optimizer = build_optimizer(model, cfg.optimizer)
 
-        if distributed:
-            if cfg.dist_params.get('linear_scale_lr', False):
-                new_lr = len(cfg.gpu_ids) * cfg.optimizer.lr
-                logger.info(f'enabled linear scaling rule to the learning rate. \
-                    changed LR from {cfg.optimizer.lr} to {new_lr}')
-                cfg.optimizer.lr = new_lr
+    if cfg.get('runner') is None:
+        cfg.runner = {'type': 'IterBasedRunner', 'max_iters': cfg.total_iters}
+        warnings.warn(
+            'config is now expected to have a `runner` section, '
+            'please set `runner` in your config.', UserWarning)
+    runner = build_runner(
+        cfg.runner,
+        default_args=dict(
+            model=model,
+            batch_processor=None,
+            optimizer=optimizer,
+            work_dir=cfg.work_dir,
+            logger=logger,
+            meta=meta))
 
-        # Save config
-        # cfg.dump(os.path.join(cfg.work_dir, 'config.yaml'))
-        # logger.info(f'Config:\n{cfg.pretty_text}')
+    fp16_cfg = cfg.get('fp16', None)
+    if fp16_cfg is not None:
+        grad_clip = cfg.optimizer_config.get('grad_clip', None)
+        cfg.optimizer_config = Fp16OptimizerHook(
+            **fp16_cfg, grad_clip=grad_clip, distributed=distributed)
 
-        if distributed:
-            os.environ['MASTER_ADDR'] = cfg.dist_params.get('master_addr', 'localhost')
-            os.environ['MASTER_PORT'] = cfg.dist_params.get('master_port', '29500')
-            mp.spawn(SegTrainer.train_worker, nprocs=len(cfg.gpu_ids),
-                     args=(target_classes, datasets, cfg, distributed, True, timestamp, meta))
-        else:
-            SegTrainer.train_worker(
-                None,
-                target_classes,
-                datasets,
-                cfg,
-                distributed,
-                True,
-                timestamp,
-                meta
-            )
+    # register hooks
+    runner.register_training_hooks(cfg.lr_config, cfg.optimizer_config,
+                                   cfg.checkpoint_config, cfg.log_config,
+                                   cfg.get('momentum_config', None))
 
-        # Save outputs
-        output_ckpt_path = os.path.join(cfg.work_dir, 'latest.pth')
-        best_ckpt_path = glob.glob(os.path.join(cfg.work_dir, 'best_mDice_*.pth'))
-        if len(best_ckpt_path) > 0:
-            output_ckpt_path = best_ckpt_path[0]
-        best_ckpt_path = glob.glob(os.path.join(cfg.work_dir, 'best_mIoU_*.pth'))
-        if len(best_ckpt_path) > 0:
-            output_ckpt_path = best_ckpt_path[0]
-        return dict(final_ckpt=output_ckpt_path)
+    # register parameters manager hook
+    params_manager_cfg = cfg.get('params_config', None)
+    if params_manager_cfg is not None:
+        runner.register_hook(build_params_manager(params_manager_cfg))
 
-    @staticmethod
-    def train_worker(gpu, target_classes, datasets, cfg, distributed=False, validate=False,
-                     timestamp=None, meta=None):
-        # logger = get_logger()
-        if distributed:
-            torch.cuda.set_device(gpu)
-            dist.init_process_group(backend=cfg.dist_params.get('backend', 'nccl'),
-                                    world_size=len(cfg.gpu_ids), rank=gpu)
-            logger.info(f'dist info world_size = {dist.get_world_size()}, rank = {dist.get_rank()}')
+    # an ugly walkaround to make the .log and .log.json filenames the same
+    runner.timestamp = timestamp
 
-        # Model
-        model = build_segmentor(cfg.model)
-        model.CLASSES = target_classes
+    for hook in cfg.get('custom_hooks', ()):
+        runner.register_hook_from_cfg(hook)
 
-        train_segmentor(
-            model,
-            datasets,
-            cfg,
-            distributed=distributed,
-            validate=True,
-            timestamp=timestamp,
-            meta=meta
-        )
+    # register eval hooks
+    if validate:
+        val_dataset = build_dataset(cfg.data.val, dict(test_mode=True))
+        val_dataloader = build_dataloader(
+            val_dataset,
+            samples_per_gpu=1,
+            workers_per_gpu=cfg.data.workers_per_gpu,
+            dist=distributed,
+            shuffle=False)
+        eval_cfg = cfg.get('evaluation', {})
+        eval_cfg['by_epoch'] = cfg.runner['type'] != 'IterBasedRunner'
+        eval_hook = DistEvalHook if distributed else EvalHook
+        # In this PR (https://github.com/open-mmlab/mmcv/pull/1193), the
+        # priority of IterTimerHook has been modified from 'NORMAL' to 'LOW'.
+        runner.register_hook(
+            eval_hook(val_dataloader, **eval_cfg), priority='LOW')
+
+    if cfg.resume_from:
+        runner.resume(cfg.resume_from)
+    elif cfg.load_from:
+        runner.load_checkpoint(cfg.load_from)
+    runner.run(data_loaders, cfg.workflow)
+
+
+# Temporary copy from mmdet
+# TODO: refactor
+def scatter_cpu(inputs):
+
+    """Scatter inputs to cpu.
+    :type:`~mmcv.parallel.DataContainer`.
+    """
+
+    def scatter_map(obj):
+        if isinstance(obj, torch.Tensor):
+            return [obj]
+        if isinstance(obj, DataContainer):
+            return obj.data
+        if isinstance(obj, tuple) and len(obj) > 0:
+            return list(zip(*map(scatter_map, obj)))
+        if isinstance(obj, list) and len(obj) > 0:
+            out = list(map(list, zip(*map(scatter_map, obj))))
+            return out
+        if isinstance(obj, dict) and len(obj) > 0:
+            out = list(map(type(obj), zip(*map(scatter_map, obj.items()))))
+            return out
+        return [obj]
+
+    # After scatter_map is called, a scatter_map cell will exist. This cell
+    # has a reference to the actual function scatter_map, which has references
+    # to a closure that has a reference to the scatter_map cell (because the
+    # fn is recursive). To avoid this reference cycle, we set the function to
+    # None, clearing the cell
+    try:
+        return scatter_map(inputs)
+    finally:
+        scatter_map = None
+
+
+def scatter_kwargs(inputs, kwargs):
+    """Scatter with support for kwargs dictionary"""
+    inputs = scatter_cpu(inputs) if inputs else []
+    kwargs = scatter_cpu(kwargs) if kwargs else []
+    if len(inputs) < len(kwargs):
+        inputs.extend([() for _ in range(len(kwargs) - len(inputs))])
+    elif len(kwargs) < len(inputs):
+        kwargs.extend([{} for _ in range(len(inputs) - len(kwargs))])
+    inputs = tuple(inputs)
+    kwargs = tuple(kwargs)
+    return inputs, kwargs
+
+
+class MMDataCPU(MMDataParallel):
+    """Implementation of MMDataParallel to use CPU for training"""
+
+    def scatter(self, inputs, kwargs):
+        return scatter_kwargs(inputs, kwargs)
+
+    def train_step(self, *inputs, **kwargs):
+        inputs, kwargs = self.scatter(inputs, kwargs)
+        return self.module.train_step(*inputs[0], **kwargs[0])
+
+    def val_step(self, *inputs, **kwargs):
+        inputs, kwargs = self.scatter(inputs, kwargs)
+        return self.module.val_step(*inputs[0], **kwargs[0])
+
+    def forward(self, *inputs, **kwargs):
+        inputs, kwargs = self.scatter(inputs, kwargs)
+        return self.module(*inputs[0], **kwargs[0])
