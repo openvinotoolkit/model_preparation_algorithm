a03a7238a202c7e0a7ad784d1e23da222209d5ba
eunwoosh <eunwoo.shin@intel.com>
2022-09-07 21:26:48 +0900

    update save_initial_weight_hook.py

--- mpa/modules/hooks/save_initial_weight_hook.py
+++ mpa/modules/hooks/save_initial_weight_hook.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
+from typing import Optional, Callable
 
 from mmcv.runner import HOOKS, Hook
 
@@ -8,16 +9,27 @@ from mmcv.runner import HOOKS, Hook
 @HOOKS.register_module()
 class SaveInitialWeightHook(Hook):
 
-    def __init__(self, save_path, **kwargs):
-        self.save_path = save_path
-        self.args = kwargs
+    def __init__(
+        self,
+        save_path: str,
+        file_name: str = "weights.pth",
+        after_save_func: Optional[Callable] = None,
+        **kwargs
+    ):
+        self._save_path = save_path
+        self._file_name = file_name
+        self._after_save_func = after_save_func
+        self._args = kwargs
 
     def before_run(self, runner):
         runner.logger.info('Saving weight before training')
         runner.save_checkpoint(
-            self.save_path,
-            filename_tmpl='weights.pth',
+            self._save_path,
+            filename_tmpl=self._file_name,
             save_optimizer=False,
             create_symlink=False,
-            **self.args
+            **self._args
         )
+
+        if self._after_save_func is not None:
+            self._after_save_func()
