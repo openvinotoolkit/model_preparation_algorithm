ad0b3fcb594e0297f9d5b2da54d8a8cf4d7bf8b4
Emily <emily.chun@intel.com>
2022-09-14 09:24:13 +0900

    Fix CVS-91469 sseg compatibility issue

---
+++ models/segmentation/_base_/ocr_litehrnet18.py
@@ -0,0 +1,74 @@
+__norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='ClassIncrSegmentor',
+    is_task_adapt=True,
+    pretrained=None,
+    backbone=dict(
+        type='LiteHRNet',
+        norm_cfg=__norm_cfg,
+        norm_eval=False,
+        extra=dict(
+            stem=dict(
+                stem_channels=32,
+                out_channels=32,
+                expand_ratio=1,
+                strides=(2, 2),
+                extra_stride=False,
+                input_norm=False,
+            ),
+            num_stages=3,
+            stages_spec=dict(
+                num_modules=(2, 4, 2),
+                num_branches=(2, 3, 4),
+                num_blocks=(2, 2, 2),
+                module_type=('LITE', 'LITE', 'LITE'),
+                with_fuse=(True, True, True),
+                reduce_ratios=(8, 8, 8),
+                num_channels=(
+                    (40, 80),
+                    (40, 80, 160),
+                    (40, 80, 160, 320),
+                )
+            ),
+            out_modules=dict(
+                conv=dict(
+                    enable=False,
+                    channels=320
+                ),
+                position_att=dict(
+                    enable=False,
+                    key_channels=128,
+                    value_channels=320,
+                    psp_size=(1, 3, 6, 8),
+                ),
+                local_att=dict(
+                    enable=False
+                )
+            ),
+            out_aggregator=dict(
+                enable=True
+            ),
+            add_input=False
+        )
+    ),
+    decode_head=dict(
+        type='FCNHead',
+        in_channels=40,
+        in_index=0,
+        channels=40,
+        input_transform=None,
+        kernel_size=1,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=-1,
+        num_classes=2,
+        norm_cfg=__norm_cfg,
+        align_corners=False,
+        enable_out_norm=False,
+        loss_decode=[
+            dict(type='CrossEntropyLoss',
+                 use_sigmoid=False,
+                 sampler=dict(type='MaxPoolingPixelSampler', ratio=0.25, p=1.7),
+                 loss_weight=1.0),
+        ]),
+)
