e443c5df6c593c4b6896a202524b1dd292b7e13e
dongkwan-kim01 <dongkwan.kim@intel.com>
2022-10-20 14:48:18 +0900

    separate CAM explainer

--- mpa/cls/explainer.py
+++ mpa/cls/explainer.py
@@ -1,51 +1,101 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
+from contextlib import nullcontext
 
-from os import path as osp
+import os.path as osp
+import numpy as np
+import torch
+
+import mmcv
+from mmcv.parallel import MMDataParallel
+from mmcv.runner import load_checkpoint, wrap_fp16_model
+
+from mmcls.datasets import build_dataloader, build_dataset
+from mmcls.models import build_classifier
 
 from mpa.registry import STAGES
-from .inferrer import ClsInferrer
-
+from mpa.cls.stage import ClsStage
+from mpa.modules.hooks.auxiliary_hooks import FeatureVectorHook, SaliencyMapHook
+from mpa.modules.utils.task_adapt import prob_extractor
 from mpa.utils.logger import get_logger
-
 logger = get_logger()
 
 
 @STAGES.register_module()
-class ClsExplainer(ClsInferrer):
+class ClsInferrer(ClsStage):
     def run(self, model_cfg, model_ckpt, data_cfg, **kwargs):
-        """Run evaluation stage
+        """Run explain stage
 
-        - Run inference
-        - Get saliency map
+        - Configuration
+        - Environment setup
+        - Run explain via auxiliary_hook
         """
-        self.eval = True
         self._init_logger()
         mode = kwargs.get('mode', 'train')
         if mode not in self.mode:
-            logger.warning(f'mode for this stage {mode}')
             return {}
 
         cfg = self.configure(model_cfg, model_ckpt, data_cfg, training=False, **kwargs)
 
-        # mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+        mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+        outputs = self._explain(cfg)
+        if cfg.get('task_adapt', False) and self.extract_prob:
+            output_file_path = osp.join(cfg.work_dir, 'pre_stage_res.npy')
+            np.save(output_file_path, outputs, allow_pickle=True)
+            return dict(pre_stage_res=output_file_path)
+        else:
+            return dict(
+                outputs=outputs
+                )
 
-        # Save config
-        cfg.dump(osp.join(cfg.work_dir, 'config.yaml'))
-        logger.info(f'Config:\n{cfg.pretty_text}')
+    def _explain(self, cfg):
+        if cfg.get('task_adapt', False) and not hasattr(self, 'eval'):
+            dataset_cfg = cfg.data.train.copy()
+            dataset_cfg.pipeline = cfg.data.test.pipeline
+            self.dataset = build_dataset(dataset_cfg)
+        else:
+            self.dataset = build_dataset(cfg.data.test)
 
-        # Inference
-        infer_results = super()._infer(cfg)
-        
-        # get saliency maps
-        saliency_maps = infer_results['saliency_maps']
-        self.dump_saliency_map('saliency_maps', saliency_maps)
-        
-    @staticmethod
-    def dump_saliency_map(root, saliency_maps):
-        from PIL import Image
-        for i, saliency_map in enumerate(saliency_maps):
-            img = Image.fromarray(saliency_map)
-            img = img.resize((224, 224))
-            img.save(osp.join(root, f'saliency_map{i}.png'))
+        # Data loader
+        data_loader = build_dataloader(
+            self.dataset,
+            samples_per_gpu=cfg.data.samples_per_gpu,
+            workers_per_gpu=cfg.data.workers_per_gpu,
+            dist=False,
+            shuffle=False,
+            round_up=False,
+            persistent_workers=False)
+
+        # build the model and load checkpoint
+        model = build_classifier(cfg.model)
+        self.extract_prob = hasattr(model, 'extract_prob')
+        fp16_cfg = cfg.get('fp16', None)
+        if fp16_cfg is not None:
+            wrap_fp16_model(model)
+        if cfg.load_from is not None:
+            logger.info('load checkpoint from ' + cfg.load_from)
+            _ = load_checkpoint(model, cfg.load_from, map_location='cpu')
+
+        model.eval()
+        model = MMDataParallel(model, device_ids=[0])
+
+        # InferenceProgressCallback (Time Monitor enable into Infer task)
+        ClsStage.set_inference_progress_callback(model, cfg)
+        feature_vectors = []
+        saliency_maps = []
+
+        with FeatureVectorHook(model.module.backbone) as fhook:
+            with SaliencyMapHook(model.module.backbone) as shook:
+                for data in data_loader:
+                    with torch.no_grad():
+                        # inference
+                        _ = model(return_loss=False, **data)
+                feature_vectors = fhook.records
+                saliency_maps = shook.records
+
+        outputs = dict(
+            feature_vectors=feature_vectors,
+            saliency_maps=saliency_maps
+        )
+        return outputs
