e962caf0a61be23ecaa7f647a2756428020a96e0
dongkwan-kim01 <dongkwan.kim@intel.com>
2022-11-04 14:04:03 +0900

    RISE will be added later

--- mpa/modules/xai/builder.py
+++ mpa/modules/xai/builder.py
@@ -1,7 +1,2 @@
-from mpa.modules.xai.explain_algorithms import RISE
-from mpa.modules.xai.explain_algorithms import A_RISE
-from mpa.modules.xai.explain_algorithms import D_RISE
-
-
 def build_explainer(model, cfg):
     pass
