e9da0fd538030bc62082418ebf96170bbe2e271e
Lee, Soobee <soobeele@intel.com>
2022-11-09 02:59:29 +0900

    enable unlabel in multi-label

--- mpa/cls/trainer.py
+++ mpa/cls/trainer.py
@@ -66,6 +66,7 @@ class ClsTrainer(ClsStage):
 
         # Data
         if 'unlabeled' in cfg.data:
+            print(cfg.data.unlabeled)
             datasets = [[build_dataset(cfg.data.train), build_dataset(cfg.data.unlabeled)]]
         else:
             datasets = [build_dataset(cfg.data.train)]
