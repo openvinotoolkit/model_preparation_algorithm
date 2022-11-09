cc9de28c96142e4252afacec7a9625735458c447
dongkwan-kim01 <dongkwan.kim@intel.com>
2022-11-09 15:52:32 +0900

    flake8_check

--- mpa/cls/inferrer.py
+++ mpa/cls/inferrer.py
@@ -110,7 +110,7 @@ class ClsInferrer(ClsStage):
 
         assert len(eval_predictions) == len(feature_vectors) == len(saliency_maps), \
                 f"Number of elements should be the same, however, number of outputs are \
-                    {len(eval_predictions)}, {len(feature_vectors)}, and {len(saliency_maps)}"
+                {len(eval_predictions)}, {len(feature_vectors)}, and {len(saliency_maps)}"
         outputs = dict(
             eval_predictions=eval_predictions,
             feature_vectors=feature_vectors,
