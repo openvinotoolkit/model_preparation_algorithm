fix: change normalization

mpa/modules/ov/ops/op.py
@@ -9,6 +9,7 @@ from typing import Optional, Tuple, Union, Generic, TypeVar, Type
 import torch
 
 from .utils import get_dynamic_shape
+from ..utils import get_op_name
 
 
 @dataclass
@@ -39,7 +40,7 @@ class Operation(torch.nn.Module, Generic[_T]):
     def from_ov(cls, ov_op):
         op_type = ov_op.get_type_name()
         op_version = ov_op.get_version()
-        op_name = ov_op.get_friendly_name().replace(".", "_")
+        op_name = get_op_name(ov_op)
         assert cls.TYPE != "" and cls.VERSION >= 0
         assert op_type == cls.TYPE
         assert op_version == cls.VERSION