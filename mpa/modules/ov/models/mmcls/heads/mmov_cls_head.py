3892e5221d541847257ed4810f6d9f48d638c48b
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2022-11-14 11:13:21 +0900

    feat: add multiheaders classifier

--- mpa/modules/ov/models/mmcls/heads/mmov_cls_head.py
+++ mpa/modules/ov/models/mmcls/heads/mmov_cls_head.py
@@ -26,15 +26,17 @@ class MMOVClsHead(ClsHead):
         ] = None,
         init_weight: bool = False,
         verify_shape: bool = True,
+        softmax_at_test: bool = True,
         **kwargs,
     ):
-        kwargs.pop("in_channels")
-        kwargs.pop("num_classes")
+        kwargs.pop("in_channels", None)
+        kwargs.pop("num_classes", None)
         super().__init__(**kwargs)
 
         self._model_path = model_path
         self._weight_path = weight_path
         self._init_weight = init_weight
+        self._softmax_at_test = softmax_at_test
 
         self.model = MMOVModel(
             model_path,
@@ -52,18 +54,17 @@ class MMOVClsHead(ClsHead):
 
     def forward_train(self, x, gt_label, **kwargs):
         cls_score = self.model(x)
+        while cls_score.dim() > 2:
+            cls_score = cls_score.squeeze(2)
         losses = self.loss(cls_score, gt_label, **kwargs)
         return losses
 
-    def simple_test(self, x, softmax=True, post_process=True):
+    def simple_test(self, x):
         cls_score = self.model(x)
         while cls_score.dim() > 2:
             cls_score = cls_score.squeeze(2)
-        if softmax:
+        if self._softmax_at_test:
             pred = F.softmax(cls_score, dim=1) if cls_score is not None else None
         else:
             pred = cls_score
-        if post_process:
-            return self.post_process(pred)
-        else:
-            return
+        return self.post_process(pred)
