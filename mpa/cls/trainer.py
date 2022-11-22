9f832a9b8406e63511e4bbab792d644657f70e18
Kim, Sungchul <sungchul.kim@intel.com>
2022-11-22 17:15:13 +0900

    disenable validation during warmstart

--- mpa/cls/trainer.py
+++ mpa/cls/trainer.py
@@ -117,16 +117,18 @@ class ClsTrainer(ClsStage):
         # cfg.dump(osp.join(cfg.work_dir, 'config.yaml')) # FIXME bug to save
         # logger.info(f'Config:\n{cfg.pretty_text}')
 
+        # TODO (sungchul): check other condition for vaildation
+        validate = False if cfg.model.type in ['BYOL'] else True
         if distributed:
             os.environ['MASTER_ADDR'] = cfg.dist_params.get('master_addr', 'localhost')
             os.environ['MASTER_PORT'] = cfg.dist_params.get('master_port', '29500')
 
             mp.spawn(ClsTrainer.train_worker, nprocs=len(cfg.gpu_ids),
-                     args=(datasets, cfg, distributed, True, timestamp, meta))
+                        args=(datasets, cfg, distributed, validate, timestamp, meta))
         else:
             ClsTrainer.train_worker(None, datasets, cfg,
                                     distributed,
-                                    True,
+                                    validate,
                                     timestamp,
                                     meta)
 
