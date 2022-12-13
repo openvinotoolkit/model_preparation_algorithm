30cc06a8d636253491dafb4bb552b5c275e46c5f
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2022-12-13 13:23:20 +0900

    chore: comment imports out

--- mpa/modules/ov/models/__init__.py
+++ mpa/modules/ov/models/__init__.py
@@ -2,26 +2,29 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# flake8: noqa
-try:
-    import mmcls as mmcls_import_test
-except ImportError:
-    pass
-else:
-    from . import mmcls
-
-try:
-    import mmdet as mmdet_import_test
-except ImportError:
-    pass
-else:
-    from . import mmdet
-
-try:
-    import mmseg as mmseg_import_test
-except ImportError:
-    pass
-else:
-    from . import mmseg
+# FIXME: comment out for the time being
+# due to annoying nncf warning messages
+#
+#  # flake8: noqa
+#  try:
+#      import mmcls as mmcls_import_test
+#  except ImportError:
+#      pass
+#  else:
+#      from . import mmcls
+#
+#  try:
+#      import mmdet as mmdet_import_test
+#  except ImportError:
+#      pass
+#  else:
+#      from . import mmdet
+#
+#  try:
+#      import mmseg as mmseg_import_test
+#  except ImportError:
+#      pass
+#  else:
+#      from . import mmseg
 
 from .ov_model import OVModel
