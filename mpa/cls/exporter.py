454d6af82133494cfaf25bb8baba6b3c09430d52
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2022-12-14 10:20:50 +0900

    chore: get rid of unused export

--- mpa/cls/exporter.py
+++ mpa/cls/exporter.py
@@ -8,15 +8,13 @@ import torch.onnx
 import traceback
 from functools import partial
 
-from mmcv.runner import load_checkpoint, wrap_fp16_model
+from mmcv.runner import load_checkpoint
 
 from mpa.registry import STAGES
 from .stage import ClsStage
-from mpa.utils import mo_wrapper
 from mpa.utils.logger import get_logger
 import numpy as np
 import torch
-from mmcls.datasets.pipelines import Compose
 logger = get_logger()
 
 
@@ -32,32 +30,15 @@ class ClsExporter(ClsStage):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def get_fake_input(self, cfg, orig_img_shape=(128, 128, 3)):
-        pipeline = cfg.data.test.pipeline
-        pipeline = Compose(pipeline)
-        data = dict(img=np.zeros(orig_img_shape, dtype=np.uint8))
-        data = pipeline(data)
-        return data
-
-    def get_norm_values(self, cfg):
-        pipeline = cfg.data.test.pipeline
-        mean_values = [0, 0, 0]
-        scale_values = [1, 1, 1]
-        for pipeline_step in pipeline:
-            if pipeline_step.type == 'Normalize':
-                mean_values = pipeline_step.mean
-                scale_values = pipeline_step.std
-                break
-        return mean_values, scale_values
-
     def run(self, model_cfg, model_ckpt, data_cfg, **kwargs):
         """Run exporter stage
 
         """
         self._init_logger()
-        mode = kwargs.get('mode', 'train')
+        logger.info("exporting the model")
+        mode = kwargs.get("mode", "train")
         if mode not in self.mode:
-            logger.warning(f'mode for this stage {mode}')
+            logger.warning(f"mode for this stage {mode}")
             return {}
 
         cfg = self.configure(model_cfg, model_ckpt, data_cfg, training=False, **kwargs)
@@ -104,15 +85,15 @@ class ClsExporter(ClsStage):
                     "msg": f"exception {type(ex)}: {ex}\n\n{traceback.format_exc()}"
                 }
 
-        bin_file = [f for f in os.listdir(output_path) if f.endswith('.bin')][0]
-        xml_file = [f for f in os.listdir(output_path) if f.endswith('.xml')][0]
-        logger.info('Exporting completed')
+        bin_file = [f for f in os.listdir(output_path) if f.endswith(".bin")][0]
+        xml_file = [f for f in os.listdir(output_path) if f.endswith(".xml")][0]
+        logger.info("Exporting completed")
         return {
-            'outputs': {
-                'bin': os.path.join(output_path, bin_file),
-                'xml': os.path.join(output_path, xml_file),
+            "outputs": {
+                "bin": os.path.join(output_path, bin_file),
+                "xml": os.path.join(output_path, xml_file),
             },
-            'msg': ''
+            "msg": "",
         }
 
     def _mmdeploy_export(self, output_path, onnx_file_name, model, cfg, deploy_cfg):
@@ -141,32 +122,3 @@ class ClsExporter(ClsStage):
 
     def _naive_export(self, output_path, onnx_file_name, model, cfg):
         raise NotImplementedError()
-
-        model.forward = partial(model.forward, img_metas={}, return_loss=False)
-
-        data = self.get_fake_input(cfg)
-        fake_img = data['img'].unsqueeze(0)
-
-        torch.onnx.export(model,
-                          fake_img,
-                          os.path.join(output_path, onnx_file_name),
-                          verbose=False,
-                          export_params=True,
-                          input_names=['data'],
-                          output_names=['logits', 'feature_vector', 'saliency_map'],
-                          dynamic_axes={},
-                          opset_version=11,
-                          operator_export_type=torch.onnx.OperatorExportTypes.ONNX
-                          )
-
-        mean_values, scale_values = self.get_norm_values(cfg)
-        mo_args = {
-            'input_model': os.path.join(output_path, onnx_file_name),
-            'mean_values': mean_values,
-            'scale_values': scale_values,
-            #  'data_type': "FP32",
-            'model_name': 'model',
-            'reverse_input_channels': None,
-        }
-
-        ret, msg = mo_wrapper.generate_ir(output_path, output_path, silent=True, **mo_args)
