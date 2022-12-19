27bf2eda2ba5afba831b51304500512c6407571f
Lee, Soobee <soobeele@intel.com>
2022-12-19 18:48:03 +0900

    global unlabeled data hook

--- mpa/modules/hooks/unlabeled_data_hook.py
+++ mpa/modules/hooks/unlabeled_data_hook.py
@@ -2,15 +2,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import importlib
 from mmcv.runner import get_dist_info
 from mmcv.runner import HOOKS, Hook
-
-from mmdet.datasets import build_dataset, build_dataloader
-
 from mpa.modules.datasets.composed_dataloader import ComposedDL
 from mpa.utils.logger import get_logger
 
 logger = get_logger()
+task_lib_name = dict(classification="mmcls", detection="mmdet", segmentation="mmseg")
 
 
 @HOOKS.register_module()
@@ -21,15 +20,22 @@ class UnlabeledDataHook(Hook):
         unlabeled_data_cfg,
         samples_per_gpu,
         workers_per_gpu,
+        model_task,
         seed=None,
         **kwargs
     ):
         super().__init__(**kwargs)
+
         # Build unlabeled dataset & loader
-        logger.info('In UnlabeledDataHook.before_epoch, creating unlabeled dataset...')
+        m = importlib.import_module(f"{task_lib_name[model_task]}.datasets")
+        build_dataset = getattr(m, "build_dataset")
+        build_dataloader = getattr(m, "build_dataloader")
+
         self.unlabeled_dataset = build_dataset(unlabeled_data_cfg)
+
         _, world_size = get_dist_info()
-        logger.info('In UnlabeledDataHook.before_epoch, creating unlabeled data_loader...')
+
+        logger.info('In UnlabeledDataHook, creating unlabeled data_loader...')
         self.unlabeled_loader = build_dataloader(
             self.unlabeled_dataset,
             samples_per_gpu,
@@ -44,8 +50,6 @@ class UnlabeledDataHook(Hook):
     def before_epoch(self, runner):
         if self.composed_loader is None:
             logger.info('In UnlabeledDataHook.before_epoch, creating ComposedDL'
-                        f'([labeled({len(runner.data_loader)}, unlabeled({len(self.unlabeled_loader)})])')
+                        f'([labeled({len(runner.data_loader.dataset)}, unlabeled({len(self.unlabeled_loader.dataset)})])')
             self.composed_loader = ComposedDL([runner.data_loader, self.unlabeled_loader])
-        # Per-epoch replacement: train-only loader -> train+unlabeled loader
-        # (It's similar to local variable in epoch. Need to update every epoch...)
         runner.data_loader = self.composed_loader
